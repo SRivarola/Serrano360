4b6b294c3c91322c37cd70b316a7fdfa46ff6bbc
Mateo Fernández Banchi <matefbanchi@gmail.com>
2022-03-25 11:02:05 -0300

    navbar y responsive

--- src/Pages/ComoTrabajamos/ComoTrabajamos.js
+++ src/Pages/ComoTrabajamos/ComoTrabajamos.js
@@ -13,23 +13,58 @@ const ComoTrabajamos = () => {
       <p>Porque además de sacar fotos profesionales, también pensamos en tu comodidad y en <br></br> proteger tu mercancía,  ya que vamos a tu empresa, local, showroom ¡donde quiera que esté!<br></br>  De esta manera no necesitas mover tus productos.</p>
       </div>
       <div className="listContainer">
-        <ul className="listIcons">
-          <li> <Icon className='icon' icon='system-uicons:contacts' /></li>
-          <li> <Icon className='icon' icon='system-uicons:coins' /></li>
-          <li> <Icon className='icon' icon='system-uicons:thumbs-up' /></li>
-          <li><Icon className='icon' icon='system-uicons:calendar-add' /></li>
-          <li><Icon className='icon' icon='ion:diamond-outline' /></li>
-          <li><Icon className='icon' icon='bi:sun' /></li>
-        </ul>
-        <ul className="listText">
-          <li> <p> Se contacta con nosotros por cualquiera de estos medios (whatsapp, mail, Instagram). Nos cuenta sobre el tipo de servicio que necesita su empresa y nos deja sus datos.</p></li>
-          <li> <p> Le enviamos un presupuesto y posible fecha de entrega del trabajo.</p></li>
-          <li> <p> Aprobación del presupuesto y 50% de seña por adelantado.</p></li>
-          <li><p>Agendamos día y hora para que un integrante de nuestro equipo te visite y comience el trabajo en tu empresa, local,  showroom, etc.</p></li>
-          <li> <p>Etapa de edición.
-            ¡Estamos creando tus fotos!</p></li>
-          <li><p>Entrega del trabajo y pago del 50% restante.</p></li>
-        </ul>
+
+        <tbody>
+          <tr>
+            <td>
+            <Icon className='icon' icon='system-uicons:contacts' />
+            </td>
+            <td>
+            <p> Se contacta con nosotros por cualquiera de estos medios (whatsapp, mail, Instagram). Nos cuenta sobre el tipo de servicio que necesita su empresa y nos deja sus datos.</p>
+            </td>
+          </tr>
+          <tr>
+          <td>
+          <Icon className='icon' icon='system-uicons:coins' />
+            </td>
+            <td>
+            <p> Le enviamos un presupuesto y posible fecha de entrega del trabajo.</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+            <Icon className='icon' icon='system-uicons:thumbs-up' />
+            </td>
+            <td>
+            <p> Aprobación del presupuesto y 50% de seña por adelantado.</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+            <Icon className='icon' icon='system-uicons:calendar-add' />
+            </td>
+            <td>
+            <p>Agendamos día y hora para que un integrante de nuestro equipo te visite y comience el trabajo en tu empresa, local,  showroom, etc.</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+            <Icon className='icon' icon='ion:diamond-outline' />
+            </td>
+            <td>
+            <p>Etapa de edición.
+            ¡Estamos creando tus fotos!</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+            <Icon className='icon' icon='bi:sun' />
+            </td>
+            <td>
+            <p>Entrega del trabajo y pago del 50% restante.</p>
+            </td>
+          </tr>
+        </tbody>
       </div>
     </div>
   )
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,30 +6,35 @@ import "./Navbar.scss";
 const Navbar = () => {
   return (
     <div className="navContainer">
+     
       <input type="checkbox" />
       <span></span>
       <span></span>
       <span></span>
-
+  
+      <section className="backgroundNavbar">
       <ul className="menu">
         <li className="first">
           <Link to="/">
             <h3> SERRANO 360°</h3>
           </Link>
         </li>
-        <li>
-          <Link to="/fotos360">Fotos 360°</Link>
-        </li>
-        <li>
-          <Link to="/sobremi">Sobre Mi</Link>
-        </li>
-        <li>
-          <Link to="/comotrabajamos">Como Trabajamos</Link>
-        </li>
-        <li>
-          <Link to="/contacto">Contacto</Link>
-        </li>
+        <div className="menu2">
+          <li>
+            <Link to="/fotos360">Fotos 360°</Link>
+          </li>
+          <li>
+            <Link to="/sobremi">Sobre mí</Link>
+          </li>
+          <li>
+            <Link to="/comotrabajamos">Como trabajo</Link>
+          </li>
+          <li>
+            <Link to="/contacto">Contacto</Link>
+          </li>
+        </div>
       </ul>
+      </section>
     </div>
   );
 };
