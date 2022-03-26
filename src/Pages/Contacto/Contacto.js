02e8ade14572400d6364dcc5546aa2f412e4c5fd
santiago rivarola <srivarola@gmail.com>
2022-03-26 20:56:52 -0300

    sobre mi

--- src/Pages/Contacto/Contacto.js
+++ src/Pages/Contacto/Contacto.js
@@ -1,6 +1,43 @@
-import React from "react";
-const Contacto = () => {
-  return <div></div>;
-};
+import React from 'react'
+import '../../index.scss'
+import './Contacto.scss'
+import '../ComoTrabajamos/ComoTrabajamos.scss'
+import { Icon } from '@iconify/react'
+import img1 from '../../assets/contacto/t_amarillas_single_1080x1080.gif'
 
-export default Contacto;
+export default function Sobremi() {
+  return (
+    <div className='container contacto'>
+      <h1>Contacto</h1>
+      <span className='subrayado'></span>
+      <div className='listContainer' >
+        <table>
+          <tbody>
+            <tr>
+              <td><Icon className="icon" icon="uil:whatsapp"/> </td>
+              <td>094 487 159</td>
+            </tr>
+            <tr>
+              <td><Icon className="icon" icon="uil:envelope"/></td>
+              <td>360serrano@gmail.com</td>
+            </tr>
+            <tr>
+              <td><Icon className="icon" icon="uil:instagram"/></td>
+              <td>serrano_360</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className='imgContainer'>
+        <img src={img1} alt='foto del produtco'/>
+      </div>
+      <div className='derechos'>
+        <p>
+          Todos los derechos reservados ©.
+          <br/>
+          Web realizada por Mateo, Santiago y Sophie.
+        </p>
+      </div>
+    </div>
+  )
+}
