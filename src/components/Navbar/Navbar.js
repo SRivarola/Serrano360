b9a358c451b23f749018fe5e6115ce79a730f5fa
santiago rivarola <srivarola@gmail.com>
2022-03-28 20:19:09 -0300

    navBar con styled

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,55 +1,90 @@
-import React, {useRef} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../../index.scss"
 import "./Navbar.scss";
+import {Icon} from '@iconify/react'
+import { BarraMenu, ItemList, Leyenda, List, Menu, ToggleButton } from './NavbarStyled'
 
 const Navbar = () => {
   
-  // const menuField = React.useRef(false)
- 
-
-  // const toggleMenu = () => {
-  //   const value = menuField.current.value;
-  //   menuField.current.value = true ? value.style.transform='none' : null
-  // }
+  const [navBar, setNavBar] = useState('false')
   
+  const handleClick = () => {
+    if(navBar === 'false'){
+      setNavBar('true')
+    } else {
+      setNavBar('false')
+    }
+  }
+
+  console.log(navBar)
 
   return (
-    <div className="navContainer">
-     
-      <input type="checkbox"  />
-      <div className="menuhamburguer">
-      <span></span>
-      <span></span>
-      <span></span>
+    <>
+      <div className="containerBtn">
+        <ToggleButton type='button' onClick={handleClick}>
+          <Icon className="icono" icon="akar-icons:three-line-horizontal"/>
+        </ToggleButton>
       </div>
-      <section className="backgroundNavbar"
-      //  ref={menuField} onClick={toggleMenu}
-       >
-         <span className="borderMenu"></span>
-      <ul className="menu">
-        <li className="first">
-          <Link to="/">
-            <h3 className="indexButton"> SERRANO 360°</h3>
-          </Link>
-        </li>
-        <div className="menu2">
-          <li>
-            <Link to="/fotos360">Fotos 360°</Link>
-          </li>
-          <li>
-            <Link to="/sobremi">Sobre mí</Link>
-          </li>
-          <li>
-            <Link to="/comotrabajamos">Como trabajo</Link>
-          </li>
-          <li>
-            <Link to="/contacto">Contacto</Link>
-          </li>
-        </div>
-      </ul>
-      </section>
-    </div>
+      <BarraMenu props={navBar}>
+        <Menu>
+          <List>
+            <ItemList>
+              <Link className="item" to="/">SERRANO 360°</Link>
+              <Leyenda>Fotografia de productos</Leyenda>
+            </ItemList>
+            <ItemList>
+              <Link to="/fotos360">Fotos 360°</Link>
+            </ItemList>
+            <ItemList>
+              <Link to="/sobremi">Sobre mí</Link>
+            </ItemList>
+            <ItemList>
+              <Link to="/comotrabajamos">Como trabajo</Link>
+            </ItemList>
+            <ItemList>
+              <Link to="/contacto">Contacto</Link>
+            </ItemList>
+          </List>
+        </Menu>
+        <div className="transparencia"></div>
+      </BarraMenu>
+    </>
+    // <div className="navContainer">
+     
+    //   <input type="checkbox"  />
+    //   <div className="menuhamburguer">
+    //   <span></span>
+    //   <span></span>
+    //   <span></span>
+    //   </div>
+    //   <section className="backgroundNavbar"
+    //   //  ref={menuField} onClick={toggleMenu}
+    //    >
+    //      <span className="borderMenu"></span>
+    //   <ul className="menu">
+    //     <li className="first">
+    //       <Link to="/">
+    //         <h3 className="indexButton"> SERRANO 360°</h3>
+    //       </Link>
+    //     </li>
+    //     <div className="menu2">
+    //       <li>
+    //         <Link to="/fotos360">Fotos 360°</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/sobremi">Sobre mí</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/comotrabajamos">Como trabajo</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/contacto">Contacto</Link>
+    //       </li>
+    //     </div>
+    //   </ul>
+    //   </section>
+    // </div>
   );
 };
 
---
+++ src/components/Navbar/NavbarStyled.js
@@ -0,0 +1,80 @@
+import styled, {css} from 'styled-components'
+
+const BarraMenu = styled.div`
+    width: 100%;
+    display: block;
+    position: fixed;
+    z-index:999;
+    @media (max-width: 768px){
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        height: 100vh;
+        /* margin-left: 100%; */
+
+        ${props => props === 'true' & css`
+            margin-left: 0% !important;
+        `}
+        ${props => props === 'false' & css`
+            margin-left: 100% !important;
+        `}
+    }
+`
+
+const Menu = styled.nav`
+    width: 100%;
+    height: 88px;
+    z-index:999;
+    background: #fff;
+    top: 0;
+    @media (max-width: 768px){
+        width: 100%;
+        height: 100%;
+        padding-top: 48px;
+    }
+`
+const ToggleButton = styled.button`
+    background: transparent;
+    border: none;
+    cursor: pointer;
+`
+
+const List = styled.ul`
+    width: 90%;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 3fr 1fr 1fr 1fr 1fr;
+    @media (max-width: 768px) {
+      display: flex;
+      flex-direction: column;
+    }
+`
+
+const ItemList = styled.li`
+    line-height: 88px;
+    text-align: center;
+    &:first-child {
+        text-align: left !important;   
+        margin-top: 0px !important;  
+        margin-bottom: 30px; 
+    }
+    @media (max-width: 768px){
+        text-align: left;
+        margin-top: -20px;
+    }
+`
+const Leyenda = styled.p`
+    display: none;
+    margin-top: -60px;
+    @media (max-width: 768px){
+        display: flex;
+    }
+`
+
+export {
+    BarraMenu,
+    Menu,
+    ToggleButton,
+    List,
+    ItemList,
+    Leyenda,
+}
