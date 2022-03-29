c80d7d229faf2329af8e1edfe2801c8858852fe1
santiago rivarola <srivarola@gmail.com>
2022-03-29 20:13:11 -0300

    fotos360

--- src/Pages/Fotos360/Fotos360.js
+++ src/Pages/Fotos360/Fotos360.js
@@ -52,33 +52,52 @@ export default function Fotos360() {
       </div>
       </div>
       :
-      <div className='verMas'>
-        <p>Fotoproductos</p>
-        <div className='gridVerMas grid1'>
-          <img src={vermas1} alt='foto del producto'/>
-          <img className='yoda' src={vermas2} alt='foto del producto'/>
-          <img className='corona' src={vermas3} alt='foto del producto'/>
+      <>
+        <div className='container fotos360'>
+          <div className='titleContainer'>
+            <h1>Fotos 360°</h1>
+            <span className='subrayado'></span>
+          </div>
+          <div>
+            <p style={{marginTop:'30px'}}>Para crear las fotografías en 360°, las mismas son tomadas desde varios ángulos con una buena iluminación, el fondo puede ser del color que prefieras. Y para hacerlas interactivas, la animación es de 360°.  La idea es buscar a la perfección las cualidades y características del objeto fotografiado.</p>
+            <p>Esta manera de presentar las fotos, permite mostrar los productos de un ecommerce desde todos los ángulos, porque cada imagen 360° está compuesta de al menos 20 fotografías diferentes del producto, que después se unen usando un software especial.</p>
+            <div className='gridFotos'>
+              <img className='img1' src={img1} alt='foto del producto' />
+              <img className='img2' src={img2} alt='foto del producto' />
+              <img className='img3' src={img3} alt='foto del producto' />
+            </div>
+            <p>Las fotografías de producto 360° animan al usuario a detenerse y experimentar con ellas, girándolas en diferentes direcciones, para hacerse una idea mejor del aspecto real del producto, y por tanto, es más probable que se animen a comprar desde cualquier lugar del país, utilizando cualquier tipo de dispositivo (computadoras, celulares, tablets, etc.).</p>
+            <p>Al contratar este servicio, tu negocio, empresa o emprendimiento se diferencia de la competencia, al  destacar y captar la atención de los posibles clientes.</p>
+          </div>
         </div>
-        <div className='gridVerMas grid2'>
-          <img src={vermas4} alt='foto del producto'/>
-          <img className='parlante' src={vermas5} alt='foto del producto'/>
+        <div className='verMas'>
+          <p>Fotoproductos</p>
+          <div className='gridVerMas grid1'>
+            <img src={vermas1} alt='foto del producto'/>
+            <img className='yoda' src={vermas2} alt='foto del producto'/>
+            <img className='corona' src={vermas3} alt='foto del producto'/>
+          </div>
+          <div className='gridVerMas grid2'>
+            <img src={vermas4} alt='foto del producto'/>
+            <img className='parlante' src={vermas5} alt='foto del producto'/>
+          </div>
+          <div className='gridVerMas grid3'>
+            <img src={vermas6} alt='foto del producto'/>
+          </div>
+          <div className='gridVerMas grid4'>
+            <img src={vermas7} alt='foto del producto'/>
+            <img className='anillo' src={vermas8} alt='foto del producto'/>
+          </div>
+          <div className='gridVerMas grid5'>
+            <img src={vermas9} alt='foto del producto'/>
+            <img className='juegoDeTe' src={vermas10} alt='foto del producto'/>
+          </div>
+          <div className='gridVerMas grid6'>
+            <img className='luna' src={vermas11} alt='foto del producto'/>
+            <img className='perfume' src={vermas12} alt='foto del producto'/>
+          </div>
         </div>
-        <div className='gridVerMas grid3'>
-          <img src={vermas6} alt='foto del producto'/>
-        </div>
-        <div className='gridVerMas grid4'>
-          <img src={vermas7} alt='foto del producto'/>
-          <img className='anillo' src={vermas8} alt='foto del producto'/>
-        </div>
-        <div className='gridVerMas grid5'>
-          <img src={vermas9} alt='foto del producto'/>
-          <img className='juegoDeTe' src={vermas10} alt='foto del producto'/>
-        </div>
-        <div className='gridVerMas grid6'>
-          <img className='luna' src={vermas11} alt='foto del producto'/>
-          <img className='perfume' src={vermas12} alt='foto del producto'/>
-        </div>
-      </div>
+      </>
     }
     </>
   )
