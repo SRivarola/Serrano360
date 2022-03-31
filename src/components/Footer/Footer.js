06818d28d2ec956e42a57ba43d145729303ebecf
Mateo Fernández Banchi <matefbanchi@gmail.com>
2022-03-31 11:30:36 -0300

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,19 +5,19 @@ import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
-  <footer className='footerContainer'>
+    <footer className='footerContainer'>
       <section className='footer'>
-          <div className='derechos'>
-            <p>
-              Todos los derechos reservados ©.
-              <br />
-              Web realizada por <Link to='https://www.linkedin.com/mateofernandezbanchi' target='_blank'> Mateo Fernández </Link> , 
-              <Link to='https://www.linkedin.com/in/santiago-rivarola-b6b500219/' target='_blank'> Santiago Rivarola </Link> y <Link to='https://www.linkedin.com/in/sofiaollerschmid/' target='_blank'> Sophia Oller Schmid </Link>
-            
-            </p>
-          </div>
-        </section>
-      
+        <div className='derechos'>
+          <p>
+            Todos los derechos reservados ©.
+            <br />
+            Web realizada por <Link to='https://www.linkedin.com/mateofernandezbanchi' target='_blank'> Mateo Fernández </Link> ,
+            <Link to='https://www.linkedin.com/in/santiago-rivarola-b6b500219/' target='_blank'> Santiago Rivarola </Link> y <Link to='https://www.linkedin.com/in/sofiaollerschmid/' target='_blank'> Sophia Oller Schmid </Link>
+
+          </p>
+        </div>
+      </section>
+
     </footer>
   )
 }
